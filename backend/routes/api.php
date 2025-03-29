964c4921c07bf269fa374e51bad2e0284c1c7831
Ricardo <richardyord@gmail.com>
2025-03-29 13:34:56 -0400

    Creacion resource y models

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AreaController;
-use App\Http\Controllers\apiK\CronogramaController;
+use App\Http\Controllers\apiK\AreaControllerK;
+use App\Http\Controllers\AreaController;
 
 // Ruta para obtener el usuario autenticado (si usas auth)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -13,4 +13,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Rutas RESTful para las áreas de competencia
 Route::apiResource('areas', AreaController::class);
 
-Route::get('/areasRegistras', [CronogramaController::class, 'getEventosCronograma']);
+Route::get('/areasRegistradas', [AreaControllerK::class, 'getEventosCronograma']);
