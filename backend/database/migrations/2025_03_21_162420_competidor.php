6576f35eef54dc999cf54dd2be809022ddfd3471
Ricardo <richardyord@gmail.com>
2025-04-10 13:55:19 -0400

    Creacion de tablas para el segundo sprint

--- backend/database/migrations/2025_03_21_162420_competidor.php
+++ backend/database/migrations/2025_03_21_162420_competidor.php
@@ -33,16 +33,16 @@ class Competidor extends Migration{
                   ->on('ubicacion')
                   ->onDelete('cascade');
             
+            $table->index('ci');
+            $table->index(['apellidos', 'nombres']);
+            $table->index('colegio_id');
+            $table->index('curso_id');
+            $table->index('ubicacion_id');
+            
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_general_ci';
             $table->engine = 'InnoDB';
         });
-
-        $table->index('ci');
-        $table->index(['apellidos', 'nombres']);
-        $table->index('colegio_id');
-        $table->index('curso_id');
-        $table->index('ubicacion_id');
     }
 
     public function down(){
