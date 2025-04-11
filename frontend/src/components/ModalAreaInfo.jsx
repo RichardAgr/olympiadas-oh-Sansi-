5cce3a93232fa92c7327a6e7b01025fd3e33580c
Ricardo <richardyord@gmail.com>
2025-04-11 17:26:25 -0400

    Correcciones en las importaciones

--- frontend/src/components/ModalAreaInfo.jsx
+++ frontend/src/components/ModalAreaInfo.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./ModalAreaInfo.css"; 
 
 const ModalAreaInfo = ({ area, onClose }) => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './routes/AppRouter';
 import './index.css';
