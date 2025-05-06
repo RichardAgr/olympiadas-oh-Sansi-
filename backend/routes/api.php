a5bf9b8a540e4f7972a3cf432bcda2807e909d16
KevinJaimesGuaman <201800135@est.umss.edu>
2025-05-05 20:03:24 -0400

    apiOcr
    un api PUT para sacar el nombre numero de comprobante y el monto

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Api\TutorController;
 use App\Http\Controllers\Api\CompetidorController;
 use App\Http\Controllers\Api\NotificacionController;
 use App\Http\Controllers\Api\EstadisticasController;
+use App\Http\Controllers\Api\ocrController;
 
 Route::get('/evento/fechas', [EventoController::class, 'listarFechasEvento']);
 Route::get('/evento/fechas/{area_id}/{tipo}', [EventoController::class, 'obtenerFechaPorTipo']);
@@ -60,3 +61,4 @@ Route::get('/tutor/VerMiPerfil/{id}', [TutorController::class, 'VerMiPerfil']);
 Route::put('/tutor/ActualizarMiPerfil/{id}', [TutorController::class, 'ActualizarMiPerfil']);
 Route::get('/tutor/VerNotificaciones/{id}',[NotificacionController::class, 'VerNotificacionesTutor']);
 Route ::post('/tutor/{id_tutor}/cambiarEstadoNotificacion/{id_notificacion}', [NotificacionController::class, 'cambiarEstadoNotificacion']);
+Route::post('/tutor/extraerOcr',[ocrController::class, 'extraerTexto']);
